1ef0376195578751a4c1a9f59adb135c3bb7430d
insungcho1939 <insungcho1939@finset.io>
2022-09-08 21:05:20 +0900

    Add styled-component

---
+++ src/styles/global.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+import { color } from '../types/color'
+
+export const GlobalStyle = styled.div`
+  margin: 0;
+  padding: 0 16px;
+  background-color: ${color.black};
+  min-height: 100vh;
+`
---
+++ src/types/color.ts
@@ -0,0 +1,19 @@
+interface IColor {
+  [key: string]: string
+}
+
+export const color: IColor = {
+  black: '#0C0F0F',
+  white: '#FFFFFF',
+  gray: '#868E9C',
+  red: '#F65900',
+  green: '#6BBF00',
+  yellow: '#FFC410',
+  yellowGreen: '#C2CF04',
+  boldYellow: '#DFC80A',
+  orange: '#FBA408',
+  boldOrange: '#F68500',
+  lightGray: '#676E7F',
+  borderColor: '#30333F',
+  boxBgColor: '#1E2127',
+}
